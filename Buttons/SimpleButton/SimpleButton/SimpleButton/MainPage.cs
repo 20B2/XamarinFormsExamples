503b781e8c33c7947a9ea65932c3b293408c0035
Binod Marikhu <bnod_468@hotmail.com>
2017-06-13 18:54:33 +0545

    Simple Button application added

---
+++ Buttons/SimpleButton/SimpleButton/SimpleButton/MainPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleButton
+{
+    
+    public class MainPage:ContentPage
+    {
+        StackLayout loggerLayout = new StackLayout();
+        private Button button;
+
+        public MainPage()
+        {
+            button = new Button
+            {
+                Text = "Click Me!!"
+            };
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    button,
+                    new ScrollView
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        Content = loggerLayout
+                    }
+                }
+            };
+
+            HandleEvents();
+        }
+
+        private void HandleEvents()
+        {
+            button.Clicked += delegate
+            {
+                loggerLayout.Children.Add(new Label
+                {
+                    Text = "Button clicked at " + DateTime.Now.ToString()
+                });
+            };
+        }
+    }
+}

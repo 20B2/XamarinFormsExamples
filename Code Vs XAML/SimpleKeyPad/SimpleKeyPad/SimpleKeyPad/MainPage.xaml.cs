a9aa33f0cc93e2deec81ded9ca2f45d1bae67dd4
Binod Marikhu <bnod_468@hotmail.com>
2017-06-21 15:01:36 +0545

    SimpleKeypad using XAML added

---
+++ Code Vs XAML/SimpleKeyPad/SimpleKeyPad/SimpleKeyPad/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleKeyPad
+{
+    public partial class MainPage : ContentPage
+    {
+        //App app = Application.Current as App;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //displayLabel.Text = app.DisplayLabelText;
+
+            backspaceButton.IsEnabled = displayLabel.Text != null &&
+                                        displayLabel.Text.Length > 0;
+        }
+
+        void OnDigitButtonClicked(object sender, EventArgs args)
+        {
+            Button button = sender as Button;
+            displayLabel.Text += button.StyleId as string;
+            backspaceButton.IsEnabled = true;
+
+            //app.DisplayLabelText = displayLabel.Text;
+        }
+        void OnBackSpaceButtonClicked(object sender, EventArgs args )
+        {
+            string text = displayLabel.Text;
+            displayLabel.Text = text.Substring(0, text.Length - 1);
+            backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+            //app.DisplayLabelText = displayLabel.Text;
+        }
+    }
+}

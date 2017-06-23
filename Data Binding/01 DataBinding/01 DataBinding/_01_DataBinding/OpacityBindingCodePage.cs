9b89e08ede0b4dfd1af7155743b3f3815881980d
Binod Marikhu <bnod_468@hotmail.com>
2017-06-23 19:44:37 +0545

    Simple OpacityLableBinding Project addded

---
+++ Data Binding/01 DataBinding/01 DataBinding/_01_DataBinding/OpacityBindingCodePage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _01_DataBinding
+{
+    public class OpacityBindingCodePage:ContentPage
+    {
+        public OpacityBindingCodePage()
+        {
+            Label label = new Label
+            {
+                Text = "Opacity Binding Demo",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
+            Slider slider = new Slider
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            //Set the binding context: target is Label; source is Slider.
+            label.BindingContext = slider;
+
+            //Bind the properties: target is Opacity; source is Value
+            label.SetBinding(Label.OpacityProperty, "Value");
+
+            //Construct the Page
+            Padding = new Thickness(10, 0);
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    label,
+                    slider,
+                }
+            };
+        }
+    }
+}
